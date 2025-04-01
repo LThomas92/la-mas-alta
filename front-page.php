83559e80f70e2f1e4a6569d0b5d3fa4981811fbe
Lawrence Thomas <lawrence@macbookpro.mynetworksettings.com>
2025-03-31 23:05:18 -0400

    updates to homepaeg

--- front-page.php
+++ front-page.php
@@ -26,6 +26,7 @@
           </figure>
           <div class="c-homepage__featured-meta-info">
           <p class="c-homepage__featured-small-title"><?php echo $smallTitle; ?></p>
+          <h2 class="c-homepage__featured-big-title"><?php echo $bigTitle; ?></h2>
           <a href="<?php echo $link['url']; ?>" class="c-homepage__featured-link"><?php echo $link['title']; ?></a> 
           </div> 
         </li>
@@ -70,9 +71,57 @@ else :
 endif; ?>
   </ul>
 
+  </div>
 
+<section class="c-homepage__best-sellers">
+  <h3 class="c-homepage__best-sellers-title">Best Sellers</h3>
+  <p class="c-homepage__best-sellers-desc">Our clothing combines timeless style with modern comfort, offering high-quality, versatile pieces that elevate your everyday wardrobe. Crafted with care and designed to last, our collection is perfect for those who seek both fashion and function.</p>
+
+  <?php if( have_rows('best_sellers') ): ?>
+
+    <ul class="c-homepage__best-sellers-list">
+    <?php while( have_rows('best_sellers') ) : the_row();
+
+        $image = get_sub_field('image');
+        $title = get_sub_field('title'); ?>
+
+      <li class="c-homepage__best-sellers-list-item">
+          <figure>
+            <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
+          </figure>
+          <h4 class="c-homepage__best-sellers-list-item-title"><?php echo $title['title']; ?></h4>
+      </li>
+        
+    <?php endwhile; ?>
+  </ul>
+
+<?php 
+else :
+endif; ?>
+
+</section>
+
+
+
+
+<section class="c-homepage__newsletter-section">
+  <?php 
+    $newsletterImage = get_field('newsletter_image', 'option');
+    $newsletterTitle = get_field('newsletter_title', 'option');
+    $newsletterDesc = get_field('newsletter_description', 'option');
+  ?>
+  <div class="c-homepage__newsletter-image">
+    <img src="<?php echo $newsletterImage['url']; ?>" alt="<?php echo $newsletterImage['alt']; ?>">
+  </div>
+  <div class="c-homepage__newsletter-content">
+    <h4 class="c-homepage__newsletter-title"><?php echo $newsletterTitle; ?></h4>
+    <p class="c-homepage__newsletter-desc"><?php echo $newsletterDesc; ?></p>
+    <div class="c-homepage__newsletter-form">
+      <?php echo do_shortcode('[mc4wp_form id=291]'); ?>
+    </div>
 
   </div>
+</section>
 
 
 
