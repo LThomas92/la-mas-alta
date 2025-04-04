f78978cf744956815e6750c9fa29c9007b51e06a
Lawrence <lthomas@administrators-iMac.local>
2025-04-04 15:00:35 -0400

    updates to single collection page

--- header.php
+++ header.php
@@ -51,7 +51,7 @@ ob_start();
   <div class="main-content">
     <section class="main-header">
 	<header id="masthead" class="site-header">
-    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Spectral:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Archivo+Narrow:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
 
     <img class="search-icon" src="<?php echo get_template_directory_uri(); ?>/img/search.svg" alt="Search Icon"/>
--- single-collections.php
+++ single-collections.php
@@ -12,9 +12,52 @@
             <h2 class="c-single-collections__header-title"><?php echo $headerTitle; ?></h2>
             <p class="c-single-collections__header-desc"><?php echo $headerDesc; ?></p>
         </div>
-        <img class="c-single-collections__image" src="<?php echo $headerImage['url']; ?>" alt="<?php echo $headerImage['alt']; ?>">
+        <div class="c-single-collections__image-container">
+            <img class="c-single-collections__image" src="<?php echo $headerImage['url']; ?>" alt="<?php echo $headerImage['alt']; ?>">
+        </div>
     </section>
 
+    <section class="c-single-collections__products-section">
+    <?php if( have_rows('products') ): ?>
+
+    <ul class="c-single-collections__products">
+    <?php while( have_rows('products') ) : the_row(); ?>
+
+        <?php $image = get_sub_field('image');
+              $cta = get_sub_field('cta');
+              $title = get_sub_field('title');
+              $desc = get_sub_field('description');
+        ?>
+        <li class="c-single-collections__product">
+            <figure>
+                <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
+            </figure>
+            <h4 class="c-single-collections__product-title"><?php echo $title; ?></h4>
+            <p class="c-single-collections__product-desc"><?php echo $desc; ?></p>
+        </li>
+
+  <?php endwhile; ?>
+    </ul>
+
+<?php 
+else :
+endif; ?>
+    </section>
+
+<section class="c-single-collections__product-details-section">
+    <?php 
+        $bgImage = get_field('product_details_image');
+        $productDetailTitle = get_field('product_details_title');
+        $productDetailDesc = get_field('product_details_description');
+    ?>
+    <div class="c-single-collections__product-details-bg-image">
+        <img src="<?php echo $bgImage['url']; ?>" alt="<?php echo $bgImage['alt']; ?>">
+    </div>
+    <div class="c-single-collections__product-details-box">
+        <h3 class="c-single-collections__product-details-title"><?php echo $productDetailTitle; ?></h3>
+        <p class="c-single-collections__product-details-desc"><?php echo $productDetailDesc; ?></p>
+    </div>
+</section>
 
 
 </div>
